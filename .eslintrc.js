268a25869ae2f91a14becfc02c0c67c8cccda411
Jonathan Throne <jthrone@skyslope.com>
2020-10-16 13:45:33 -0700

    Added in a few things.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true
+    env: {
+        es6: true,
     },
-    parser: "@typescript-eslint/parser", // specifies the ESLint parser
+    parser: '@typescript-eslint/parser', // specifies the ESLint parser
     extends: [
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
     ],
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: 'module',
     },
     rules: {
         // place to specify ESLint rules that may overwrite specified rules.
-    }
+    },
 };
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,16 @@ const app: Koa<DefaultState, DefaultContext> = new Koa();
 
 app.use(bodyParser());
 
-const router: Router<any, {}> = new Router();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+    ctx.body = { msg: 'Hello World!' };
+
+    await next();
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log('Koa started :3000');
+});
