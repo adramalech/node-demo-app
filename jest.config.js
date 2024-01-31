bd4f3ccceb60a82e81b6e17841105062b5f11e63
Jonathan Throne <adramalech707@gmail.com>
2024-01-31 14:31:38 -0800

    Fixed stuff.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 var config = {
     automock: true,
     collectCoverageFrom: ['**/*.ts'],
